f5eae194961f4e6100ff8269217ae78f649f4990
Andrii Romanov <andrii.romanov@dat.com>
2023-02-23 10:32:42 +0200

    Task 3: created lambda functions

---
+++ tests/getProductsList.test.ts
@@ -0,0 +1,11 @@
+import { getProductsList } from '../src/getProductsList';
+
+describe('Get list of products test', () => {
+    test('Should return array of products and status 200.', async () => {
+        const res = await getProductsList();
+        const decodedRes = JSON.parse(res.body);
+
+        expect(decodedRes.length).toBeGreaterThan(0);
+        expect(res.statusCode).toBe(200);
+    });
+});
