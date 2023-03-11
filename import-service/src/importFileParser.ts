b605c59e81f53d956534080143a745d3b3150581
Andrii Romanov <andrii.romanov@dat.com>
2023-03-11 21:31:23 +0200

    updated importFileParses lambda

--- import-service/src/importFileParser.ts
+++ import-service/src/importFileParser.ts
@@ -1,16 +1,17 @@
-import { S3 } from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import { Readable } from 'stream'
 import csv from 'csv-parser';
 
 export const importFileParser = async (event) => {
-  const client = new S3({ region: 'eu-west-2', httpOptions: { timeout: 3000 } });
+  const client = new S3({ region: 'eu-west-2' });
   const stream = new Readable();
   const params = {
     Bucket: 'import-service-bucket-aws-js',
     Key: `${event.Records[0].s3.object.key}`
   };
-  const data = (await (client.getObject(params).promise())).Body.toString('utf-8')
-  stream.push(data);
+  const getObjectResult = await client.getObject(params);
+  const bodyStream = await getObjectResult.Body.transformToString();
+  stream.push(bodyStream);
   stream.push(null);
   stream.pipe(csv())
     .on('data', (data) => {
@@ -19,16 +20,10 @@ export const importFileParser = async (event) => {
     .on('end', async () => {
       const params = {
         Bucket: 'import-service-bucket-aws-js',
-        CopySource: encodeURI(`import-service-bucket-aws-js/${event.Records[0].s3.object.key}`),
+        CopySource: encodeURI(`${event.Records[0].s3.object.key}`),
         Key: event.Records[0].s3.object.key.replace('uploaded', 'parsed')
       };
-      await client.copyObject(params).promise()
-
-      const deleteparams = {
-        Bucket: 'import-service-bucket-aws-js',
-        Key: encodeURI(`import-service-bucket-aws-js/${event.Records[0].s3.object.key}`)
-      };
-
-      await client.deleteObject(deleteparams).promise();
+      await client.copyObject(params);
+      await client.deleteObject(params);
     });
 };
