579f6f40f113260ea780f49c02e2eb95d4476bea
Andrii Romanov <andreygorogogo@gmail.com>
2023-03-15 12:20:19 +0200

    Task 3: Created lambda functions to return products (#1)
    * created lambdas to return products
    
    * added swagger documentation
    
    * added unit tests
    
    * added npm scripts
    
    ---------
    
    Co-authored-by: Andrii Romanov <andrii.romanov@dat.com>

---
+++ src/functions/getProductById/handler.ts
@@ -0,0 +1,18 @@
+import type { APIGatewayProxyEvent } from "aws-lambda"
+import { formatJSONResponse, formatJSONError } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+import { getProducts } from '@libs/mocks/products';
+
+export const getProductById = async (event: APIGatewayProxyEvent) => {
+  const { productId } = event.pathParameters;
+  const products = await getProducts.then(data => data);
+  const product = products.find(p => p.id === productId);
+
+  if (!product) {
+    return formatJSONError({ message: 'Product not found!' }, 404);
+  }
+
+  return formatJSONResponse(product);
+};
+
+export const main = middyfy(getProductById);
--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -4,9 +4,20 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response: any) => {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     statusCode: 200,
     body: JSON.stringify(response)
+  }
+}
+
+export const formatJSONError = (message: any, statusCode: number) => {
+  return {
+    statusCode,
+    body: JSON.stringify(message)
   }
 }
