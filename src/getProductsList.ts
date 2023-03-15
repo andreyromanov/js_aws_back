635adff16eea3f934d47379267e2fb8576e00baf
Andrii Romanov <andreygorogogo@gmail.com>
2023-03-15 12:37:47 +0200

    Task 4: Connected to DynamoDB (#3)
    * Task 3: created lambda functions
    
    * Task 4: connected to DynamoDB
    
    * added validation and transaction
    
    ---------
    
    Co-authored-by: Andrii Romanov <andrii.romanov@dat.com>

--- src/getProductsList.ts
+++ src/getProductsList.ts
@@ -1,14 +1,39 @@
-import { getProducts } from './products';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 export const getProductsList = async () => {
-  let products = await getProducts.then(data => data)
+  console.log(`GET - getProductsList`);
+  try {
+    const dynamoDbClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+    const products = await dynamoDbClient.send(new ScanCommand({ TableName: 'products-table-dev' }));
+    const stocks = await dynamoDbClient.send(new ScanCommand({ TableName: 'stocks-table-dev' }));
+    const joinedProducts = [];
 
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
+    products.Items.forEach((product) => {
+      stocks.Items.forEach((stock) => {
+        if (product.id === stock.product_id) {
+          const { count } = stock;
+          joinedProducts.push({
+            ...product,
+            count
+          });
+        }
+      });
+    });
+
+    return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      statusCode: 200,
+      body: JSON.stringify(joinedProducts),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: error.message,
+    }
+  }
 };
