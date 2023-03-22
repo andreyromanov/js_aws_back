13279ee57bb89d635c4045115207b4675319f096
Andrii Romanov <andrii.romanov@dat.com>
2023-03-22 13:06:06 +0200

    Handle batch of items, added env file

--- product-service/src/catalogBatchProcess.ts
+++ product-service/src/catalogBatchProcess.ts
@@ -8,37 +8,35 @@ const SNS = new AWS.SNS();
 export const catalogBatchProcess = async (event): Promise<string> => {
   return new Promise((resolve, reject) => {
     console.log(`SQS - catalogBatchProcess`);
-    const parsedBody = JSON.parse(event.Records[0].body)
-    const payload = JSON.stringify({body: parsedBody})
-    console.log('STRING BODY', payload);
-    
-    const params = {
-      FunctionName: process.env.CREATE_PRODUCTS_LAMBDA,
-      Payload: payload,
-    };
-    lambda.invoke(params, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      SNS.publish({
-        Subject: 'Product created!',
-        Message: 'New product is created',
-        MessageAttributes: {
-          title: {
-            DataType: 'String',
-            StringValue: parsedBody.title,
-          },
-        },
-        TopicArn: process.env.SNS_ARN
-      }, (err, data) => {
-        if(err){
-          console.log('Error', err);
+    event.Records.forEach(record => {
+      const parsedBody = JSON.parse(record.body)
+      const payload = JSON.stringify({ body: parsedBody })
+
+      const params = {
+        FunctionName: process.env.CREATE_PRODUCTS_LAMBDA,
+        Payload: payload,
+      };
+      lambda.invoke(params, (err) => {
+        if (err) {
+          reject(err);
+          return;
         }
-        console.log('Sent mesage to topic', data);
+        SNS.publish({
+          Subject: 'Product created!',
+          Message: 'New product is created',
+          MessageAttributes: {
+            title: {
+              DataType: 'String',
+              StringValue: parsedBody.title,
+            },
+          },
+          TopicArn: process.env.SNS_ARN
+        }, (err) => {
+          if (err) {
+            console.log('Error', err);
+          }
+        });
       });
-      resolve(JSON.stringify(data));
     });
-    
   });
 };
